2879edc949f9b4dd5be8922d63fd76cc2b695b1f
unknown <meysam.reablehub@gmail.com>
2023-12-05 17:21:21 +0330

    Add entites-entities configurations-add migration

---
+++ IranCodingWebAPI/src/Infrastructure/ConfigurationServices.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Persistence;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class ConfigurationServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            return services;
+        }
+    }
+}
---
+++ IranCodingWebAPI/src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using System.Reflection;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.HasDefaultSchema(DomainSchema.Schema);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
